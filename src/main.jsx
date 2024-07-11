05812fafee5d2e3ec0be7d385cd49f5e1cb44b5d
mrThapaGannu <gannu2436@gmail.com>
2024-07-11 20:01:02 +0545

    Second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { QueryClientProvider } from "react-query";
-import queryClient from "./queryClient";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
