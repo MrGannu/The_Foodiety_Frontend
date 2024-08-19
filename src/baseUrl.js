7d1aa3c3fc250b1cef3f9769c96eb6adc8b84334
Manish Thapa <gitlabthapa@gmail.com>
2024-08-19 18:46:15 +0545

    fixed issues

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,3 +1,4 @@
-const baseURL = "http://127.0.0.1:8000/api";
+// const baseURL = "http://127.0.0.1:8000/api";
 // const baseURL = "https://thefoodiety.com/foodiety-backend/public/api";
+const baseURL = "https://thefoodiety.com/admin/The_Foodiety_Backend/public/api";
 export default baseURL;
--- src/imagebaseUrl.js
+++ src/imagebaseUrl.js
@@ -1,3 +1,5 @@
-const imageBaseURL = "http://127.0.0.1:8000/";
+// const imageBaseURL = "http://127.0.0.1:8000/";
 // const imageBaseURL = "https://thefoodiety.com/foodiety-backend/public/";
+const imageBaseURL =
+  "https://thefoodiety.com/admin/The_Foodiety_Backend/public/";
 export default imageBaseURL;
